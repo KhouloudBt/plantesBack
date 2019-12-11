5fea94048d9d8e2952b9fab8e6e9879358ff1d74
khouloud <khouloud.bentaoues33@gmail.com>
2019-12-11 14:53:46 +0100

    initial commit

---
+++ src/main/java/com/soa/plantes/PlantesApplication.java
@@ -0,0 +1,13 @@
+package com.soa.plantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soa/plantes/PlantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soa.plantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
